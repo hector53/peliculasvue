119c4c886a603b565519f7e91e1b9b155bcef65e
hector53 <hectoracosta5@gmail.com>
2020-10-30 14:48:58 -0400

    vue5

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy4'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:hector53/peliculasvue.git master:gh-pages
+git push -f git@github.com:hector53/peliculasvue.git master:gh-pages
 
 cd -
